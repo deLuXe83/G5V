41cdca6c2e2ac4736adc5d0df33a67e6653f97ea
deLuXe83 <37928770+deLuXe83@users.noreply.github.com>
2024-02-10 17:16:04 +0100

    Update NewMatchForm.vue

--- src/components/NewMatchForm.vue
+++ src/components/NewMatchForm.vue
@@ -76,7 +76,7 @@
             <v-select
               v-model="newMatchData.team2_id"
               :items="teams"
-              item-text="name"
+              item-text="name + ' ID:'"
               item-value="id"
               :rules="[
                 v => !!v || $t('CreateMatch.TeamRequired'),
